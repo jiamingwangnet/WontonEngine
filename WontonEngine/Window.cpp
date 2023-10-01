28dbb01a9f6d698b148739b1d66e17b98412702f
jiamingwangnet <jiamingwang.net@gmail.com>
2023-10-01 20:38:21 +1100

    Backface culling test

--- WontonEngine/Window.cpp
+++ WontonEngine/Window.cpp
@@ -79,6 +79,11 @@ void won::priv::Window::InitContext()
 	// alpha blending
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// backface culling
+	/*glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);*/
 }
 
 void won::priv::Window::SwapBuffer()
