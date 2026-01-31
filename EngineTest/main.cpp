2107a98d54e8046e346d36df78ea7761f587384a
jiamingwangnet <jiamingwang.net@gmail.com>
2026-01-31 22:29:06 +1100

    Made switching between screen size easier

--- EngineTest/main.cpp
+++ EngineTest/main.cpp
@@ -14,7 +14,19 @@ int main()
 	std::vector<won::Scene*> scenes{&mainScene, &kmScene};
 
 	// creates a 800x600 window with an OpenGL context resolution of 256x224
-	won::Game game{ 800, 600, "Engine Test", (won::WinFlags)won::WinFlags::Resizable, won::Color{30, 30, 30, 255}, preload, scenes, false, 0.0f, 60.0f, 256, 224 };
+	/*static constexpr int WINDOW_X = 800;
+	static constexpr int WINDOW_Y = 600;
+	static constexpr int CONTEXT_X = 256;
+	static constexpr int CONTEXT_Y = 224;*/
+
+	// creates a 1280x720 window with an OpenGL context resolution of 1280x720
+	static constexpr int WINDOW_X = 1280;
+	static constexpr int WINDOW_Y = 720;
+	static constexpr int CONTEXT_X = 1280;
+	static constexpr int CONTEXT_Y = 720;
+
+	
+	won::Game game{ WINDOW_X, WINDOW_Y, "Engine Test", (won::WinFlags)won::WinFlags::Resizable, won::Color{30, 30, 30, 255}, preload, scenes, false, 0.0f, 60.0f, CONTEXT_X, CONTEXT_Y };
 	
 	game.Start();
 
